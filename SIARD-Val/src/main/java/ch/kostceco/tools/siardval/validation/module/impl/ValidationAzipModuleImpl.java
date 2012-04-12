60f36c8c9a9bb9d7ab6ce6d49d990eda228b7e63
Chlara <chlara@gmx.ch>
2012-04-12 14:36:52 +0200

    ModulA verbessert, neu wird nicht nach extension geprüft ob es sich um eine ZIP-Datei handelt, sondern die Datei muss mit PK.. beginnen inkl. bessere Fehlermeldung

--- SIARD-Val/src/main/java/ch/kostceco/tools/siardval/validation/module/impl/ValidationAzipModuleImpl.java
+++ SIARD-Val/src/main/java/ch/kostceco/tools/siardval/validation/module/impl/ValidationAzipModuleImpl.java
@@ -1,6 +1,6 @@
 /*== SIARD-Val ==================================================================================
 The SIARD-Val application is used for validate SIARD-Files. 
-Copyright (C) 2012 Claire Röthlisberger (KOST-CECO), Martin Kaiser (KOST-CECO), XYZ (xyz)
+Copyright (C) 2012 Claire RÃ¶thlisberger (KOST-CECO), Martin Kaiser (KOST-CECO), XYZ (xyz)
 -----------------------------------------------------------------------------------------------
 SIARD-Val is a development of the KOST-CECO. All rights rest with the KOST-CECO. 
 This application is free software: you can redistribute it and/or modify it under the 
@@ -16,7 +16,10 @@ Boston, MA 02110-1301 USA or see <http://www.gnu.org/licenses/>.
 
 package ch.kostceco.tools.siardval.validation.module.impl;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.util.Arrays;
 import java.util.List;
 
 import ch.kostceco.tools.siardval.service.ConfigurationService;
@@ -29,10 +32,10 @@ import ch.enterag.utils.zip.FileEntry;
 /**
  * Validierungsschritt A (Lesbarkeit)
  * Kann die SIARD-Datei gelesen werden? 
- * valid --> lesbare und nicht passwortgeschützte ZIP-Datei oder ZIP64-Datei 
+ * valid --> lesbare und nicht passwortgeschÃ¼tzte ZIP-Datei oder ZIP64-Datei 
  * valid --> unkomprimierte ZIP64-Datei oder unkomprimierte ZIP-Datei
  * ==> Bei den Module A, B, C und D wird die Validierung abgebrochen, sollte das Resulat invalid sein!
- * @author Rc Claire Röthlisberger, KOST-CECO
+ * @author Rc Claire RÃ¶thlisberger, KOST-CECO
  */
 
 public class ValidationAzipModuleImpl extends ValidationModuleImpl implements ValidationAzipModule {
@@ -49,19 +52,49 @@ public class ValidationAzipModuleImpl extends ValidationModuleImpl implements Va
     	
         boolean valid = false;
 
-        // die Datei muss für dieses Modul mit siard, zip oder zip64 enden
-        if (! (siardDatei.getAbsolutePath().toLowerCase().endsWith(".siard") || 
-        	   siardDatei.getAbsolutePath().toLowerCase().endsWith(".zip") || 
-               siardDatei.getAbsolutePath().toLowerCase().endsWith(".zip64") )) {
-        	// Im Modul A werden alle 3 Extensions akzeptiert. Damit jedoch die SIARD-Datei als solche erkannt wird,
-        	// wird im Modul I die extension .siard geprüft
+
+        // Die Datei muss mit PK.. beginnen
+        FileReader fr = null;
+        try { 
+        	fr = new FileReader(siardDatei);
+            BufferedReader read = new BufferedReader(fr);
+        	
+        	// Hex 03 in Char umwandeln
+        	String str3= "03";
+        	int i3= Integer.parseInt(str3,16);
+        	char c3 = (char)i3;
+        	// Hex 04 in Char umwandeln
+        	String str4= "04";
+        	int i4= Integer.parseInt(str4,16);
+        	char c4 = (char)i4;
+
+        	// auslesen der ersten 4 Zeichen der Datei
+        	int length;
+        	int i;
+        	char[] buffer = new char[4];
+        	length=read.read(buffer);
+        	for(i=0;i!=length;i++);
+        	
+        	// die beiden charArrays (soll und ist) mit einander vergleichen
+        	char[] charArray1 = buffer;
+        	char[] charArray2 = new char[]{'P','K',c3,c4};
+
+            if (Arrays.equals(charArray1,charArray2)) {
+            	// hÃ¶chstwahrscheinlich ein ZIP da es mit 504B0304 respektive PK.. beginnt
+                valid = true;
+            } else{
+                getMessageService().logError(getTextResourceService().getText(MESSAGE_MODULE_A) + 
+                        getTextResourceService().getText(MESSAGE_DASHES) + 
+                        getTextResourceService().getText(ERROR_MODULE_A_INCORRECTFILEENDING));    
+                //Die SIARD-Datei ist kein ZIP.            
+                return false;
+            }
+        } catch (Exception e) {
             getMessageService().logError(getTextResourceService().getText(MESSAGE_MODULE_A) + 
-                    getTextResourceService().getText(MESSAGE_DASHES) + 
-                    getTextResourceService().getText(ERROR_MODULE_A_INCORRECTFILEENDING));    
-            //Die SIARD-Datei ist kein ZIP.            
+                    getTextResourceService().getText(MESSAGE_DASHES) + e.getMessage());                
             return false;
         }
-
+       
         // die Datei darf kein Directory sein
         if (siardDatei.isDirectory()) {
             getMessageService().logError(getTextResourceService().getText(MESSAGE_MODULE_A) + 
@@ -75,9 +108,9 @@ public class ValidationAzipModuleImpl extends ValidationModuleImpl implements Va
         Zip64File zf = null;
         try { 
         	Integer compressed = 0;
-            // Versuche das ZIP file zu öffnen
+            // Versuche das ZIP file zu Ã¶ffnen
             zf = new Zip64File(siardDatei);
-            // auslesen der Komprimierungsmethode aus allen FileEntries der zip64-Datei
+            // auslesen der Komprimierungsmethode aus allen FileEntries der zip(64)-Datei
 			List<FileEntry> fileEntryList = zf.getListFileEntries();
             for (FileEntry fileEntry : fileEntryList) {
               compressed = fileEntry.getMethod() + compressed;
