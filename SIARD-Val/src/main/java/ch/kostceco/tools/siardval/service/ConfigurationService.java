b3bccddcf4f5a6502ef17fd49463c2c1c7f42a84
Chlara <chlara@gmx.ch>
2012-06-07 11:53:28 +0200

    kleinere Bereinigungen ====================== - Aktualisierung der SIARD-Val-Hauptkommentaren - Aktualisierung configuration (metadata.xsd und SIARDVal.conf.xml) - Löschen von alten ungebrauchten Bestandteilen

--- SIARD-Val/src/main/java/ch/kostceco/tools/siardval/service/ConfigurationService.java
+++ SIARD-Val/src/main/java/ch/kostceco/tools/siardval/service/ConfigurationService.java
@@ -1,7 +1,9 @@
 /*== SIARD-Val ==================================================================================
+/*== SIARD-Val ===================================================================================
 The SIARD-Val application is used for validate SIARD-Files. 
-Copyright (C) 2012 Claire Röthlisberger (KOST-CECO), Martin Kaiser (KOST-CECO), XYZ (xyz)
------------------------------------------------------------------------------------------------
+Copyright (C) 2012 Claire Röthlisberger (KOST-CECO), Martin Kaiser (KOST-CECO), Christian Eugster,
+Olivier Debenath
+--------------------------------------------------------------------------------------------------
 SIARD-Val is a development of the KOST-CECO. All rights rest with the KOST-CECO. 
 This application is free software: you can redistribute it and/or modify it under the 
 terms of the GNU General Public License as published by the Free Software Foundation, 
@@ -12,7 +14,7 @@ See the follow GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with this program; 
 if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
 Boston, MA 02110-1301 USA or see <http://www.gnu.org/licenses/>.
-==============================================================================================*/
+==================================================================================================*/
 
 package ch.kostceco.tools.siardval.service;
 
