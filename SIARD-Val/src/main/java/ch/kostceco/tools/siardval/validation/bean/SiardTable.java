2a8f6428f4a054277b1e7fcf0ebfc8fc79aeb6c9
Olivier Debenath <olivier@debenath.ch>
2012-06-10 22:12:32 +0200

    Module E & SiardTable Bean hinzugefügt

---
+++ SIARD-Val/src/main/java/ch/kostceco/tools/siardval/validation/bean/SiardTable.java
@@ -0,0 +1,37 @@
+package ch.kostceco.tools.siardval.validation.bean;
+
+import java.util.List;
+
+import org.jdom2.Element;
+
+public class SiardTable {
+	
+	private String tableName;
+	private List<Element> metadataXMLElements;
+	private List<Element> tableXSDElements;
+	
+	public String getTableName() {
+		return tableName;
+	}
+	
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+	
+	public List<Element> getMetadataXMLElements() {
+		return metadataXMLElements;
+	}
+	
+	public void setMetadataXMLElements(List<Element> metadataXMLElements) {
+		this.metadataXMLElements = metadataXMLElements;
+	}
+	
+	public List<Element> getTableXSDElements() {
+		return tableXSDElements;
+	}
+	
+	public void setTableXSDElements(List<Element> tableXSDElements) {
+		this.tableXSDElements = tableXSDElements;
+	}
+	
+}
